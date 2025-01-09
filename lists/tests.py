bd19c210baa8a60406938568ccc5a6c591f90e01
Aidar Darmesh <darmesh.aidar@gmail.com>
2025-01-09 22:30:23 +0500

    --first unit test and view function

--- lists/tests.py
+++ lists/tests.py
@@ -6,7 +6,7 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        html = response.content.encode("utf8")
+        html = response.content.decode("utf8")
         self.assertIn("<title>To-Do lists</title>", html)
         self.assertTrue(html.startswith("<html>"))
         self.assertTrue(html.endswith("</html>"))
